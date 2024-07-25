bfabbc04f6fe562311f6a541ab8e1b8db53de2ff
jhpa0308 <jhpa0308@gmail.com>
2024-07-25 17:55:24 +0900

    [Feature] 질문컨트롤러에 질문 모델 불러오기

---
+++ src/controllers/questionController.js
@@ -0,0 +1,5 @@
+const ProvideQuestion = require("../models/provideQuestion");
+const Question = require("../models/question");
+const Comment = require("../models/comment");
+
+const { Sequelize } = require("sequelize");
