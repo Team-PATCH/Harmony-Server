af3ca72f24504a6bd6d593e7804b3a1eeee2f176
jhpa0308 <jhpa0308@gmail.com>
2024-07-25 18:24:08 +0900

    [Feature] app.js에 질문 라우터로 접근하는 코드 추가

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const db = require('./models');
+
 const mcRouter = require('./routes/mcRoutes');
+const questionRouter = require('./routes/questionRoutes');
 
 const app = express();
 const port = process.env.PORT;
@@ -18,7 +21,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/mc', mcRouter)
+app.use('/mc', mcRouter);
+app.use('/qc', questionRouter);
 
 app.get('/', (req, res) => {
   res.send('엔드포인트임 이게 나온다면 뭔가 문제가 있다')
