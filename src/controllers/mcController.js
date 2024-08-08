1346e7c4a6150777438049866699f03531239c04
sion <hbs09222@gmail.com>
2024-08-08 19:24:16 +0900

    [Feat] 추억카드 APNs 로직 추가

--- src/controllers/mcController.js
+++ src/controllers/mcController.js
@@ -6,6 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const uploadImage = require('../utils/uploadImage');
 // const uploadAudio = require('../utils/uploadAudio');
 const { upload, uploadAudio } = require('../utils/uploadAudio');
+const { notifyChatMessage, notifyNewMemoryCard } = require('./notificationController');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -164,6 +165,9 @@ const saveChatHistory = async (req, res) => {
         });
       }
     }
+    
+    // 채팅 메시지 알림 발송
+    await notifyChatMessage(chatSession);
 
     res.status(200).json({ 
       status: true, 
@@ -467,6 +471,9 @@ const createMemoryCard = async (req, res) => {
               });
           }
       }
+
+      // 새 메모리 카드 생성 알림 발송
+      await notifyNewMemoryCard(newMemoryCard);
 
       res.status(201).json({
           status: true,
