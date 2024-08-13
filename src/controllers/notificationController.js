adbd075164484010f7122aa87d54b0f7f86d3c57
WaterBean <gkstnqls9@gmail.com>
2024-08-13 15:52:11 +0900

    [Fix] 병합 시 잘못 합친  부분(쉼표,순서  등) 수정

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -182,7 +182,7 @@ module.exports = {
   notifyNewComment,
   notifyAnswerUpdate,
   notifyNewMemoryCard,
-  notifyChatMessage
+  notifyChatMessage,
   notifyNewRoutine,
   notifyRoutineUpdate,
   notifyVIPRoutineCompletion,
